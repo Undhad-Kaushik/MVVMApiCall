7b2609f6ea500d95694a52b8007fd9bf25f4e30d
Undhad Kaushik <kpkushpatel8@gmail.com>
2023-06-08 03:05:01 +0530

    Initial Commit

---
+++ MVVMApiCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMApiCall
+//
+//  Created by undhad kaushik on 08/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
